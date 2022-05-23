f1dec67fd931a82a68fb1133f7a0e033d1328a5d
7y1o <tdesu.webdesign@gmail.com>
2022-05-23 06:47:29 +0300

    1.1.1

--- src/utils/def-config.ts
+++ src/utils/def-config.ts
@@ -1,14 +1,14 @@
-import { IQRConfig } from "../interfaces/QReactor";
+import { IQRConfig } from '../interfaces/QReactor';
 
 /** Default QReactor initialization options */
 const qrDefConfig: IQRConfig = {
-    port: 4000,
-    cors: false,
-    session: {
-        secret: 'l0lk3k',
-        resave: true,
-        saveUninitialized: true
-    }
+  port: 4000,
+  cors: false,
+  session: {
+    secret: 'l0lk3k',
+    resave: true,
+    saveUninitialized: true,
+  },
 };
 
 export default qrDefConfig;
